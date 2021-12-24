ec118c27ce74f870bfe0b518f0c989a3240f6681
dkc1019 <79237108+dkc1019@users.noreply.github.com>
2021-12-24 14:25:09 -0500

    Add files via upload

---
+++ Loan_Mngmnt_Sys.py
@@ -0,0 +1,72 @@
+def master_data():
+    c_master_data = [
+         {"cs_start":100,"cs_end":199,"loan_amt_start":10000,"loan_amt_end":19999,"interest":5  ,"duration":65}
+        ,{"cs_start":400,"cs_end":499,"loan_amt_start":10000,"loan_amt_end":19999,"interest":3.5,"duration":65}
+        ,{"cs_start":200,"cs_end":299,"loan_amt_start":10000,"loan_amt_end":19999,"interest":4.5,"duration":65}
+        ,{"cs_start":300,"cs_end":399,"loan_amt_start":10000,"loan_amt_end":19999,"interest":4  ,"duration":65}
+        ]
+    return c_master_data
+    
+def chk_cust_qualify(CreditScore,LoanAmount):
+    c_master_data=[]
+    c_master_data=master_data()
+    #print(c_master_data)
+    reject=0
+    all_cs=[]
+    all_loan_amt=[]
+    string='' 
+    for chk in c_master_data :
+        all_cs.append(chk['cs_start'])
+        all_cs.append(chk['cs_end'])
+        all_loan_amt.append(chk['loan_amt_start'])
+        all_loan_amt.append(chk['loan_amt_end'])
+
+    max_cs=max(all_cs)
+    max_loan_amt=max(all_loan_amt)
+    min_cs=min(all_cs)
+    min_loan_amt=min(all_loan_amt)
+    #print('Credit Score should be between',min_cs,'and',max_cs)
+    #print('Loan Amount should be between',min_loan_amt,'and',max_loan_amt)
+
+    if (CreditScore>max_cs or CreditScore<min_cs) and (LoanAmount>max_loan_amt or LoanAmount<min_loan_amt) :
+        #print('CreditScore :',CreditScore,'and LoanAmount :', LoanAmount,' are not enough for the loan')
+        reject=1
+        string='Credit Score and LoanAmount do not fit in the criteria'
+    elif CreditScore>max_cs or CreditScore<min_cs :
+        #print('CreditScore :',l_CreditScore,'is not enough')
+        reject=1
+        string='Credit Score does not fit in the criteria'
+    elif LoanAmount>max_loan_amt or LoanAmount<min_loan_amt :
+        #print('LoanAmount :', l_LoanAmount,'is not enough')
+        string ='Your Loan Amount does not fit in the criteria'
+        reject=1
+
+    return [reject,string]
+
+
+
+def Calculate_TotalLoanAmount(CreditScore,LoanAmount) :
+    c_master_data=master_data()
+    reject=chk_cust_qualify(CreditScore,LoanAmount)[0]
+    String=chk_cust_qualify(CreditScore,LoanAmount)[1]
+    
+    if reject==0 :   
+        for c in c_master_data:
+    #print(c)
+    #print(c['cs_start'])
+            if CreditScore>=c['cs_start'] and CreditScore<=c['cs_end'] and LoanAmount>=c['loan_amt_start'] and LoanAmount<=c['loan_amt_end']:
+                TotalAmount = LoanAmount + (LoanAmount/100)*c['interest']
+                print('You have to pay', TotalAmount,'for',c['duration'],'months and interest rate will be',c['interest'],'%')
+               
+    else :
+        print(String)
+"""                
+l_CustomerName='A'
+l_CreditScore=305
+l_LoanAmount=10029
+
+Calculate_TotalLoanAmount(l_CreditScore,l_LoanAmount)
+"""
+  
+#reject=chk_cust_qualify(300,109)
+#print(reject)
